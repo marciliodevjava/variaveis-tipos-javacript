a679dae102f647635fd4a53646139b6ddd24cb14
marciliodevjava <marciliodevjava@gmail.com>
2022-05-21 11:20:32 -0300

    Atualização

--- palindromo/script.js
+++ palindromo/script.js
@@ -14,9 +14,11 @@ function verificaPalindromo2(string) {
     if(!string) return "String é inexistente";
 
     for(let i = 0; i < string.length / 2; i ++){
-        if(string[i] !== string.length -1 - i){
+        if(string[i] !== string[string.length -1 - i]){
             return false;
         }
     }
     return true;
 }
+
+console.log(verificaPalindromo2("abba"));
