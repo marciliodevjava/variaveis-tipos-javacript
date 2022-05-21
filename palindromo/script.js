bdd9cd76aaec5585f765f33cd78b03c9fcaf58ae
marciliodevjava <marciliodevjava@gmail.com>
2022-05-21 11:18:21 -0300

    Atualização

--- palindromo/script.js
+++ palindromo/script.js
@@ -1,5 +1,22 @@
+//solução n: 1
 function verificaPalindromo(string) {
-    if(!string) return;
+    if(!string) return "Essa string inexistente";
 
-    return string.split("").reverse().join("") == string;
+    return string.split("").reverse().join("") === string;
+}
+
+let myVar = false;
+
+console.log(verificaPalindromo("ovo"))
+
+//solução n:2
+function verificaPalindromo2(string) {
+    if(!string) return "String é inexistente";
+
+    for(let i = 0; i < string.length / 2; i ++){
+        if(string[i] !== string.length -1 - i){
+            return false;
+        }
+    }
+    return true;
 }
